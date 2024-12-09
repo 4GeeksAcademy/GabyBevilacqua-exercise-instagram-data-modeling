86e0cc72bfb55235c005ee709b94c5f34777732c
Gabriella Bevilacqua <bevi.gaby@gmail.com>
2024-12-09 15:10:23 +0000

    faltan relationships

--- src/models.py
+++ src/models.py
@@ -1,29 +1,64 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'User'
+    userID = Column(Integer, primary_key=True)
+    username = Column(String(50), index=True, nullable=False)    # >-< DirectMessage.SenderID
+    email = Column(String(50), nullable=False, unique=True)
+    name = Column(String(50), nullable=False)
+    lastName = Column(String(50), nullable=False)
+    password = Column(String(50), nullable=False)
+    profilePicture = Column(String(50), nullable=False)
+    createdAt = Column(DateTime)
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    postID = Column(Integer, ForeignKey(User.userID), primary_key=True)
+    userID = Column(Integer, ForeignKey(User.userID))
+    image = Column(String(100), nullable=False)
+    caption = Column(String(250))
+    createdAt = Column(DateTime)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    commentID = Column(Integer, ForeignKey(User.userID), primary_key=True)
+    postID = Column(Integer, ForeignKey(Post.postID))
+    userID = Column(Integer, ForeignKey(User.userID))
+    commentTest = Column(Integer, ForeignKey(Post.postID))
+    createdAt = Column(DateTime)
+
+class Like (Base):
+    __tablename__ = 'like'
+    likeID = Column(Integer, primary_key=True)
+    postID = Column(Integer, ForeignKey(Post.postID))
+    userID = Column(Integer, ForeignKey(User.userID))
+    createdAt = Column(DateTime)
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    followerID = Column(Integer, primary_key=True)
+    userID = Column(Integer, ForeignKey(User.userID))
+    followerUserID = Column(Integer, ForeignKey(User.userID))
+    followedAT = Column(DateTime)
+
+class DirectMessage(Base):
+    __tablename__ = 'directMessage'
+    messageID = Column(Integer, primary_key=True)
+    senderID = Column(Integer, ForeignKey(User.userID))
+    receiver = Column(Integer, ForeignKey(User.userID))
+    messageText = Column(String(250))
+    createdAt = Column(DateTime)
 
     def to_dict(self):
         return {}
